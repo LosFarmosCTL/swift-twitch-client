90d4f7266db79a0498f662e6442f50cd04c96306
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2023-02-25 00:24:17 +0000

    build: add supported platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "swift-twitch-client",
+  platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)],
   products: [
     .library(
       name: "Twitch",
