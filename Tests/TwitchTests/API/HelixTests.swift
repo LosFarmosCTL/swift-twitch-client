64aeb07bba594856158d64c262fb44bcddea443e
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2023-03-01 00:00:21 +0000

    tests: add test for helix authentication headers

--- Tests/TwitchTests/API/HelixTests.swift
+++ Tests/TwitchTests/API/HelixTests.swift
@@ -1,8 +1,16 @@
+import Foundation
+import Mocker
 import XCTest
 
 @testable import Twitch
 
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
 class HelixTests: XCTestCase {
+  var mockingURLSession: URLSession!
+
   func testHelixInitialization() {
     XCTAssertNoThrow(
       try Helix(authentication: .init(oAuth: "abcdefg", clientID: "123456789")))
@@ -18,5 +26,47 @@ class HelixTests: XCTestCase {
         )
       }
     }
+  }
+
+  override func setUpWithError() throws {
+    let configuration = URLSessionConfiguration.default
+    configuration.protocolClasses = [MockingURLProtocol.self]
+    mockingURLSession = URLSession(configuration: configuration)
+  }
+
+  func testHelixAuthentication() async throws {
+    let oAuth = "abcdefg"
+    let clientID = "123456"
+
+    let helix = try Helix(
+      authentication: .init(oAuth: oAuth, clientID: clientID),
+      urlSession: mockingURLSession)
+
+    let url = URL(string: "https://api.twitch.tv/helix/test")!
+    var mock = Mock(
+      url: url, dataType: .json, statusCode: 200, data: [.get: Data()])
+
+    mock.onRequestHandler = OnRequestHandler(requestCallback: { request in
+      guard let clientIDHeader = request.value(forHTTPHeaderField: "Client-Id")
+      else { return XCTFail("Helix request must contain a Client-Id header.") }
+
+      guard
+        let authenticationHeader = request.value(
+          forHTTPHeaderField: "Authorization")
+      else {
+        return XCTFail("Helix request must contain an Authorization header.")
+      }
+
+      XCTAssertEqual(
+        clientIDHeader, clientID,
+        "Helix request must contain the correct Client-Id header.")
+      XCTAssertEqual(
+        authenticationHeader, "Bearer " + oAuth,
+        "Helix request must contain the correct Authorization header.")
+    })
+
+    mock.register()
+
+    _ = try await helix.request(.get("test"))
   }
 }
