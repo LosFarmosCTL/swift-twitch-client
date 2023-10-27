2c284af275c64e7f1b8c13db846798560c539bbd
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2023-10-27 13:49:48 +0000

    build: add websocket-kit as a dependency for linux

--- Package.swift
+++ Package.swift
@@ -11,9 +11,17 @@ let package = Package(
       url: "https://github.com/WeTransfer/Mocker.git",
       .upToNextMajor(from: "3.0.1")),
     .package(url: "https://github.com/MahdiBM/TwitchIRC", from: "1.5.0"),
+    .package(url: "https://github.com/vapor/websocket-kit", from: "2.14.0"),
   ],
   targets: [
-    .target(name: "Twitch", dependencies: ["TwitchIRC"]),
+    .target(
+      name: "Twitch",
+      dependencies: [
+        "TwitchIRC",
+        .product(
+          name: "WebSocketKit", package: "websocket-kit",
+          condition: .when(platforms: [.linux])),
+      ]),
     .testTarget(
       name: "TwitchTests", dependencies: ["Twitch", "Mocker"],
       resources: [.process("API/MockResources")]),
