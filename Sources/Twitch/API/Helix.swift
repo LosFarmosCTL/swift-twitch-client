f72ad7520397c8cc3ae8043cff3a208e64b01f65
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-01-27 16:46:11 +0100

    refactor: extract more methods in Helix.swift

--- Sources/Twitch/API/Helix.swift
+++ Sources/Twitch/API/Helix.swift
@@ -10,47 +10,35 @@ public final class Helix {
   private let authentication: TwitchCredentials
   private let session: URLSession
 
+  private let encoder = JSONEncoder()
+  private let decoder = JSONDecoder()
+
   public init(authentication: TwitchCredentials, urlSession: URLSession? = nil) throws {
     guard authentication.clientID != nil else { throw HelixError.missingClientID }
 
     self.authentication = authentication
     self.session = urlSession ?? URLSession(configuration: .default)
+
+    self.encoder.dateEncodingStrategy = .iso8601
+    self.decoder.dateDecodingStrategy = .iso8601
   }
 
   internal func request<T: Decodable>(
     _ request: HelixRequest, with queryItems: [URLQueryItem]? = nil,
     jsonBody: Encodable? = nil
-  ) async throws -> [T] {
-    let (method, endpoint) = request.unwrap()
+  ) async throws -> (result: [T], cursor: String?) {
+    var urlRequest = self.buildURLRequest(request, queryItems: queryItems)
 
-    var urlComponents = URLComponents(string: endpoint)
-
-    if let queryItems = queryItems, !queryItems.isEmpty {
-      urlComponents?.queryItems = queryItems
-    }
-
-    let url = urlComponents?.url(relativeTo: self.baseURL)
-
-    guard let url else { fatalError("Invalid URL") }
-
-    var urlRequest = URLRequest(url: url)
-    urlRequest.httpMethod = method
     urlRequest.allHTTPHeaderFields = try? authentication.httpHeaders()
 
     if let jsonBody {
       urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-      urlRequest.httpBody = try JSONEncoder().encode(jsonBody)
+      urlRequest.httpBody = try encoder.encode(jsonBody)
     }
 
     let data = try await self.send(urlRequest)
 
-    let result = try? JSONDecoder().decode(HelixData<T>.self, from: data).data
-
-    guard let result else {
-      throw HelixError.invalidResponse(rawResponse: String(decoding: data, as: UTF8.self))
-    }
-
-    return result
+    return try self.decode(data)
   }
 
   private func send(_ request: URLRequest) async throws -> Data {
@@ -61,7 +49,7 @@ public final class Helix {
     }
 
     guard httpResponse.statusCode == 200 else {
-      let error = try? JSONDecoder().decode(TwitchError.self, from: data)
+      let error = try? decoder.decode(TwitchError.self, from: data)
 
       guard let error else {
         let rawResponse = String(decoding: data, as: UTF8.self)
@@ -75,8 +63,42 @@ public final class Helix {
 
     return data
   }
+
+  private func decode<T: Decodable>(_ data: Data) throws -> ([T], String?) {
+    let helixData = try? decoder.decode(HelixData<T>.self, from: data)
+    let result = helixData?.data
+    let cursor = helixData?.pagination?.cursor
+
+    guard let result else {
+      throw HelixError.invalidResponse(rawResponse: String(decoding: data, as: UTF8.self))
+    }
+
+    return (result, cursor)
+  }
+
+  private func buildURLRequest(_ request: HelixRequest, queryItems: [URLQueryItem]? = nil)
+    -> URLRequest
+  {
+    let (method, endpoint) = request.unwrap()
+
+    var urlComponents = URLComponents(string: endpoint)
+
+    if let queryItems = queryItems, !queryItems.isEmpty {
+      urlComponents?.queryItems = queryItems
+    }
+
+    let url = urlComponents?.url(relativeTo: self.baseURL)
+    guard let url else { fatalError("Invalid URL") }
+
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpMethod = method
+
+    return urlRequest
+  }
 }
 
+// the FoundationNetworking implementation for Linux doesn't have
+// async support yet so we need to implement it ourselves
 #if canImport(FoundationNetworking)
   extension URLSession {
     fileprivate func data(for request: URLRequest) async throws -> (Data, URLResponse) {
