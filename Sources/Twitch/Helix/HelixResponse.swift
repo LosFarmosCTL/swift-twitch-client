a8e4cd8856856535e0bf9b389f1145e23b110cf1
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-03-04 05:36:34 +0100

    fix: add missing values to helixresponse

--- Sources/Twitch/Helix/HelixResponse.swift
+++ Sources/Twitch/Helix/HelixResponse.swift
@@ -6,6 +6,21 @@ internal struct HelixResponse<T: Decodable>: Decodable {
   let points: Int?
   let template: String?
 
+  let cost: Int?
+  let totalCost: Int?
+  let maxTotalCost: Int?
+
+  enum CodingKeys: String, CodingKey {
+    case data
+    case pagination
+    case total
+    case points
+    case template
+    case cost
+    case totalCost = "total_cost"
+    case maxTotalCost = "max_total_cost"
+  }
+
   internal struct Pagination: Decodable { let cursor: String? }
 }
 
