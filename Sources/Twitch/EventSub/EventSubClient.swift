5d5fdea9a53f7f53174d45802b438d62204b160c
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-05-20 23:07:05 +0200

    fix: classic Linux  FoundationNetworking fix

--- Sources/Twitch/EventSub/EventSubClient.swift
+++ Sources/Twitch/EventSub/EventSubClient.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
 internal class EventSubClient {
   private static let maxSubscriptionsPerConnection = 300
 
