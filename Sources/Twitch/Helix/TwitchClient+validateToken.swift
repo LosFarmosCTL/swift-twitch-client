fdf5c36806c7ba0e8c117c0b079b61ab76adc51f
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-08-29 05:21:36 +0200

    fix: add FoundationNetworking import to validateToken

--- Sources/Twitch/Helix/TwitchClient+validateToken.swift
+++ Sources/Twitch/Helix/TwitchClient+validateToken.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
 extension TwitchClient {
   public static func validateToken(token: String) async throws -> ValidatedToken {
     let urlSession = URLSession(configuration: .default)
