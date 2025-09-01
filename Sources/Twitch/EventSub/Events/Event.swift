c5c7615ec010868344b8142d85961c0a3a5370a4
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-09-01 06:03:30 +0200

    fix: add missing automod event cases

--- Sources/Twitch/EventSub/Events/Event.swift
+++ Sources/Twitch/EventSub/Events/Event.swift
@@ -1,6 +1,13 @@
 public protocol Event: Decodable, Sendable {}
 
 internal enum EventType: String, Decodable {
+  // Automod
+  case automodMessageHold = "automod.message.hold"
+  case automodMessageUpdate = "automod.message.update"
+  case automodSettingsUpdate = "automod.settings.update"
+  case automodTermsUpdate = "automod.terms.update"
+
+  // Channel
   case channelFollow = "channel.follow"
   case chatMessage = "channel.chat.message"
   case channelUpdate = "channel.update"
@@ -10,6 +17,11 @@ internal enum EventType: String, Decodable {
 
   var event: Event.Type {
     switch self {
+    case .automodMessageHold: return AutomodMessageHoldEvent.self
+    case .automodMessageUpdate: return AutomodMessageUpdateEvent.self
+    case .automodSettingsUpdate: return AutomodSettingsUpdateEvent.self
+    case .automodTermsUpdate: return AutomodTermsUpdateEvent.self
+
     case .channelFollow: return ChannelFollowEvent.self
     case .chatMessage: return ChatMessageEvent.self
     case .channelUpdate: return ChannelUpdateEvent.self
