fffa420358997765ad78207ac28f96fa8f281235
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-09-04 00:21:11 +0200

    refactor: introduce helper method for mock data fetching

--- Tests/TwitchTests/EventSub/MockedMessages.swift
+++ Tests/TwitchTests/EventSub/MockedMessages.swift
@@ -2,57 +2,34 @@ import Foundation
 
 @testable import Twitch
 
-struct MockedMessages {
+enum MockedMessages {
   // Helix response for creating a subscription
-  static let mockEventSubSubscription = String(
-    data: Bundle.module.url(
-      forResource: "mockEventSubSubscription", withExtension: "json")!.data,
-    encoding: .utf8)!
+  static let mockEventSubSubscription = fromResource("mockEventSubSubscription")
 
   // MARK: - EventSub Messages
 
-  static let welcomeMessage = String(
-    data: Bundle.module.url(forResource: "welcomeMessage", withExtension: "json")!.data,
-    encoding: .utf8)!
-
-  static let welcomeMessage1SecondKeepalive = String(
-    data: Bundle.module.url(
-      forResource: "welcomeMessage1SecondKeepalive", withExtension: "json")!.data,
-    encoding: .utf8)!
-
-  static let mockRevocationMessage = String(
-    data: Bundle.module.url(forResource: "revocationMessage", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
-
-  static let mockReconnectMessage = String(
-    data: Bundle.module.url(forResource: "reconnectMessage", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
-
-  static let mockEventMessage = String(
-    data: Bundle.module.url(forResource: "mockEventMessage", withExtension: "json")!.data,
-    encoding: .utf8)!
+  static let welcomeMessage = fromResource("welcomeMessage")
+  static let welcomeMessage1SecondKeepalive = fromResource(
+    "welcomeMessage1SecondKeepalive")
+  static let mockRevocationMessage = fromResource("revocationMessage")
+  static let mockReconnectMessage = fromResource("reconnectMessage")
+  static let mockEventMessage = fromResource("mockEventMessage")
 
   // MARK: - Automod Messages
 
-  static let automodMessageHold = String(
-    data: Bundle.module.url(forResource: "automodMessageHold", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
+  static let automodMessageHold = fromResource("automodMessageHold")
+  static let automodMessageUpdate = fromResource("automodMessageUpdate")
+  static let automodSettingsUpdate = fromResource("automodSettingsUpdate")
+  static let automodTermsUpdate = fromResource("automodTermsUpdate")
 
-  static let automodMessageUpdate = String(
-    data: Bundle.module.url(forResource: "automodMessageUpdate", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
+  // MARK: - Channel Messages
 
-  static let automodSettingsUpdate = String(
-    data: Bundle.module.url(forResource: "automodSettingsUpdate", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
+  static let channelBitsUse = fromResource("channelBitsUse")
+  static let channelCheer = fromResource("channelCheer")
 
-  static let automodTermsUpdate = String(
-    data: Bundle.module.url(forResource: "automodTermsUpdate", withExtension: "json")!
-      .data,
-    encoding: .utf8)!
+  static private func fromResource(_ name: String) -> String {
+    String(
+      data: Bundle.module.url(forResource: name, withExtension: "json")!.data,
+      encoding: .utf8)!
+  }
 }
